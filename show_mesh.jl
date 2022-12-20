71b5aa972488056c9d063636ae318fa3ee954024
Petr Krysl <pkrysl@ucsd.edu>
2022-12-19 19:23:33 -0800

    update graphic scripts

--- show_mesh.jl
+++ show_mesh.jl
@@ -12,40 +12,42 @@ using FinEtools.MeshImportModule
 using FinEtoolsMultimaterialVibEP: solve_ep
 using FinEtools.MeshExportModule.VTKWrite: vtkwrite
 
+let
 # The name of the parameter file is up to you
-parameterfile = "Heat_105.json"
+    parameterfile = "Finned_81mm_Max_12kHz.json"
 # parameterfile = "twoblocks.json"
-parameters = open(parameterfile, "r") do file
-    JSON.parse(file)
-end
+    parameters = open(parameterfile, "r") do file
+        JSON.parse(file)
+    end
 
-meshfilebase, ext = splitext(parameters["meshfile"])
-output = MeshImportModule.import_NASTRAN(with_extension(meshfilebase, ext))
-fens = output["fens"]
-@info "$(count(fens)) nodes"
-@info "$(length(output["fesets"])) materials"
-for i in axes(output["fesets"], 1)
-    fes = output["fesets"][i]
-    File =  meshfilebase * "-part-$i.vtu"
-    vtkwrite(File, fens, fes)
-end
+    meshfilebase, ext = splitext(parameters["meshfile"])
+    output = MeshImportModule.import_NASTRAN(with_extension(meshfilebase, ext))
+    fens = output["fens"]
+    @info "$(count(fens)) nodes"
+    @info "$(length(output["fesets"])) materials"
+    for i in axes(output["fesets"], 1)
+        fes = output["fesets"][i]
+        File =  meshfilebase * "-part-$i.vtu"
+        vtkwrite(File, fens, fes)
+    end
 
-allfes = output["fesets"][1]
-for i in 2:length(output["fesets"])
-    fes = output["fesets"][i]
-    global allfes = cat(allfes, fes)
-end
+    allfes = output["fesets"][1]
+    for i in 2:length(output["fesets"])
+        fes = output["fesets"][i]
+        allfes = cat(allfes, fes)
+    end
 
-bfes = meshboundary(allfes)
-File =  meshfilebase * "-boundary.vtu"
-vtkwrite(File, fens, bfes)
+    bfes = meshboundary(allfes)
+    File =  meshfilebase * "-boundary.vtu"
+    vtkwrite(File, fens, bfes)
 
-obfes = outer_surface_of_solid(fens, bfes)
-File =  meshfilebase * "-outer-boundary.vtu"
-vtkwrite(File, fens, obfes)
+    obfes = outer_surface_of_solid(fens, bfes)
+    File =  meshfilebase * "-outer-boundary.vtu"
+    vtkwrite(File, fens, obfes)
 
 # mode = 7
 # scattersysvec!(u, v[:,mode])
 # File =  meshfilebase * ".vtu"
 # vtkwrite(File, fens, fes)
 # vtkwrite(File, fens, allfes; vectors=[("mode$mode", u.values)])
+end
